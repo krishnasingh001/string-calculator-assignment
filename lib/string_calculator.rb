f7dbe26916cff6b8eb65b411ba8872b22b62b855
Krishna Singh <ks452399@gmail.com>
2024-08-22 20:28:15 +0530

    Restrict negative numbers (#7)
    * Add the spec scenario for the negative numbers
    
    * Raise an error if the input string contains the negative numbers
    
    * Use the .positive? method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,13 +5,18 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
 
+    raise_negative_number_error(input) if contains_negative?(input)
     sum_of_split_values(input)
   end
 
   private
 
   def sum_of_split_values(input)
-    newline_to_comma(input).split(delimiters(input)).map(&:to_i).sum
+    numbers(input).sum
+  end
+
+  def numbers(input)
+    newline_to_comma(input).split(delimiters(input)).map(&:to_i)
   end
 
   def newline_to_comma(input)
@@ -20,5 +25,17 @@ class StringCalculator
 
   def delimiters(input)
     input.start_with?('//') ? input[2] : ','
+  end
+
+  def contains_negative?(input)
+    negative_numbers(input).count.positive?
+  end
+
+  def negative_numbers(input)
+    numbers(input).select(&:negative?)
+  end
+
+  def raise_negative_number_error(input)
+    raise "Negative numbers not allowed: #{negative_numbers(input).join(', ')}"
   end
 end
