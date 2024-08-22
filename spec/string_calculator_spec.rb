22dbb6dc1636ec3ef42902918d2d8b13a1f66d4b
Krishna Singh <krishna@lawlytics.com>
2024-08-22 18:36:45 +0530

    Spec scenarios to handle the single digit input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,23 @@ describe StringCalculator do
     it 'returns the 0 for an empty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    context 'single number' do
+      it 'returns 0 for 0' do
+        expect(calculator.add('0')).to eq(0)
+      end
+
+      it 'returns 3 for 3' do
+        expect(calculator.add('3')).to eq(3)
+      end
+
+      it 'returns 10 for 10' do
+        expect(calculator.add('10')).to eq(10)
+      end
+
+      it 'returns 110 for 110' do
+        expect(calculator.add('110')).to eq(110)
+      end
+    end
   end
 end
