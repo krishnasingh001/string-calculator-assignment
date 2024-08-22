3cf9a86f31d0dd0afb0f64a1cc41da5de34554a3
Krishna Singh <krishna@lawlytics.com>
2024-08-22 18:24:54 +0530

    Fix RuboCop warning

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# StringCalculator performs arithmetic by summing numbers from a string input
 class StringCalculator
   def add
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'string_calculator'
 
 describe StringCalculator do
